3a2fdbcc8770153ea47f4a934c600692d43d5343
Raphael Nestler <raphael.nestler@gmail.com>
2015-08-04 09:59:27 +0200

    Move get and set commands into own functions

--- src/main.rs
+++ src/main.rs
@@ -35,26 +35,34 @@ fn main() {
     let client = Client::new();
 
     if args.cmd_get {
-        let mut res = client.get(&(con_string + "login=p:admin:admin&p=l"))
-            .send().unwrap();
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-        println!("Response: {:?}, {}", res.status, body);
+        get_port_list(&con_string, &client);
     } else {
-        // create a port list "xxxx" where 'u' means leave as is, '1' enable and '0' disable
-        let mut port_list = vec!{'u', 'u', 'u', 'u'};
-        let value = if args.cmd_on { '1' } else {'0'};
-        match args.arg_port.unwrap() {
-            p @ 1usize ... 4usize => port_list[p-1] = value,
-            _ => panic!("port is out of range"),
-        };
-        let port_list: String = port_list.into_iter().collect();
-        let command = format!("{}l=p:admin:admin&p={}", con_string, port_list);
-        println!("{}", command);
-        let mut res = client.get(&command).send().unwrap();
-        let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-        println!("Response: {:?}, {}", res.status, body);
+        set_port(&con_string, &client, args.arg_port.unwrap(), args.cmd_on);
     }
+}
+
+fn get_port_list(con_string: &str, client: &Client) -> () {
+    let mut res = client.get(&(con_string.to_string() + "login=p:admin:admin&p=l"))
+        .send().unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+    println!("Response: {:?}, {}", res.status, body);
+}
+
+fn set_port(con_string: &str, client: &Client, port: usize, state: bool) {
+    // create a port list "xxxx" where 'u' means leave as is, '1' enable and '0' disable
+    let mut port_list = vec!{'u', 'u', 'u', 'u'};
+    let value = if state { '1' } else {'0'};
+    match port {
+        p @ 1usize ... 4usize => port_list[p-1] = value,
+        _ => panic!("port is out of range"),
+    };
+    let port_list: String = port_list.into_iter().collect();
+    let command = format!("{}l=p:admin:admin&p={}", con_string, port_list);
+    println!("{}", command);
+    let mut res = client.get(&command).send().unwrap();
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+    println!("Response: {:?}, {}", res.status, body);
 }
 
